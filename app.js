543271d1489ce288b643b92bb593c72623af07b6
Andrew <cuznetsofandrey@yandex.ru>
2021-04-21 17:57:07 +0300

    init extreact

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const config = require('config');
+const sequelize = require('sequelize');
+const { Op } = require("sequelize");
 
 // Database
 const db = require('./config/database');
 
 const app = express();
+
+app.use('/api/auth', require('./routes/auth.routes'));
 
 const PORT = config.get('port') || 5000;
 
@@ -19,10 +23,22 @@ db.authenticate()
 
 db.sync()
 
-models.R1022.findAll({where:{utv: "1"}, raw: true })
+models.R1022.findAll({
+        where: {
+            [Op.and]: [
+                sequelize.where(sequelize.fn('char_length', sequelize.col('p00')), 10),
+                {
+                    p00: {
+                        [Op.regexp]: '^(0|13)'
+                    }
+                }
+            ]
+        }
+    })
     .then(res=>{
         console.log(res);
-    }).catch(err=>console.log(err));
+    })
+    .catch(err=>console.log(err));
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
